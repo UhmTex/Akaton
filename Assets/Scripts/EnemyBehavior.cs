d77085922efc46c2f96edea40a64ccc26e3eff4d
Diasiris123 <ilia.tyupa@gmail.com>
2023-10-23 22:17:02 +0300

    EnemyChange
    Now enemy detecs and attacks the player

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
     [SerializeField] private float WalkSpeed;
+    public float ChaseSpeed = 6f;
 
     public Transform[] WalkPoints;
+    [SerializeField] int _numFound;
+    [SerializeField] Transform _interactionPoint;
+    [SerializeField] float _interactionPointRadius = 1f;
+    [SerializeField] LayerMask _interactableMask;
+
+    private readonly Collider[] _colliders = new Collider[3];
 
     private Transform CurrentWalkPoint;
 
-    private bool isPlayerDetected;
+    private bool isPlayerDetected = false;
+
 
     private void Start()
     {
@@ -18,12 +28,53 @@ public class EnemyBehavior : MonoBehaviour
     }
 
     private void Update()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, CurrentWalkPoint.TransformPoint(CurrentWalkPoint.position), WalkSpeed * Time.deltaTime);
+    {       
+        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
+        DetectAndAttack();
 
-        if (Vector3.Distance(transform.position, CurrentWalkPoint.TransformPoint(CurrentWalkPoint.position)) < 1f)
+        if (!isPlayerDetected )
         {
-            CurrentWalkPoint = WalkPoints[Random.Range(0, WalkPoints.Length)];
+            transform.position = Vector3.MoveTowards(transform.position, CurrentWalkPoint.position, WalkSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, CurrentWalkPoint.position) < 1f)
+            {
+                CurrentWalkPoint = WalkPoints[Random.Range(0, WalkPoints.Length)];
+            }
+        }
+
+    }
+
+    public void DetectAndAttack()
+    {
+        if (_numFound > 0)
+        {
+            var interactable = _colliders[0].gameObject;
+            if (interactable.CompareTag("Player"))
+            {
+               isPlayerDetected = true;
+               var step = ChaseSpeed * Time.deltaTime;
+               transform.position = Vector3.MoveTowards(transform.position, interactable.transform.position, step);
+               
+            }
+            else
+            {
+                isPlayerDetected = false;
+            }
+        }
+        else
+            isPlayerDetected = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            Debug.Log("Game Over");
         }
     }
 }
