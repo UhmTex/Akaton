e1f279295be9e667c3cb51a90a4b10b2de38438f
UhmTex-dev <texroblox@gmail.com>
2023-10-23 12:19:34 +0300

    Overhauled Character Movement

--- Assets/Scripts/MovementHandler.cs
+++ Assets/Scripts/MovementHandler.cs
@@ -1,42 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementHandler : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private float MovementSpeed;
-    [SerializeField] private float RotationSpeed = 180;
-    [SerializeField] private Camera cam;
+    public CharacterController CharController;
+    public Transform cam;
 
-    public GameObject BodyMesh;
+    [SerializeField] float MovementSpeed = 6;
+    [SerializeField] float JumpHeight = 5;
 
-    private Vector3 rotation;
+    private bool isGrounded;
 
-    private float Horizontal { get; set; }
-    private float Vertical { get; set; }
+    // Movement Variables
+    private float worldGravity;
+
+    private Vector3 playerVelocity;
+
+    private float smoothTurnTime = 0.1f;
+    private float smoothTurnVelocity;
+
+    private float horizontal;
+    private float vertical;
+    private Vector3 direction;
+
+    private float directionAngle;
+    private float dampAngle;
+
+    private void Start()
+    {
+        worldGravity = Physics.gravity.y - 15;
+    }
 
     private void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal");
-        Vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
 
-        rotation = new Vector3(Horizontal * RotationSpeed * Time.deltaTime, 0, 0);
-    }
+        isGrounded = CharController.isGrounded;
 
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector3(Horizontal * MovementSpeed, 0, Vertical * MovementSpeed) * Time.deltaTime;
-        
+        direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        if (Vertical < 0)
+        if (direction.magnitude > 0.1f)
         {
-            BodyMesh.transform.rotation = Quaternion.Euler(BodyMesh.transform.eulerAngles.x, cam.transform.eulerAngles.y - 90, BodyMesh.transform.eulerAngles.z);
+            directionAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            dampAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, directionAngle, ref smoothTurnVelocity, smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f, dampAngle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, directionAngle, 0f) * Vector3.forward;
+            CharController.Move(moveDir * MovementSpeed * Time.deltaTime);
         }
-        else
+
+        if (isGrounded && playerVelocity.y < 0)
         {
-            BodyMesh.transform.rotation = Quaternion.Euler(BodyMesh.transform.eulerAngles.x, cam.transform.eulerAngles.y + 90, BodyMesh.transform.eulerAngles.z);
+            playerVelocity.y = 0f;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerVelocity.y += MathF.Sqrt(JumpHeight * -1.5f * worldGravity);
+        }
+
+        playerVelocity.y += worldGravity * Time.deltaTime;
+        CharController.Move(playerVelocity * Time.deltaTime);
     }
 
 }
