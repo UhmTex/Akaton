cae6349abbc27353488f824a7f66ce7e86655536
UhmTex-dev <texroblox@gmail.com>
2023-10-29 20:21:43 +0200

    update to lightning

--- Assets/Scripts/LightningBehavior.cs
+++ Assets/Scripts/LightningBehavior.cs
@@ -8,13 +8,55 @@ public class LightningBehavior : MonoBehaviour
 {
     [SerializeField] private VisualEffect lightningEffect;
 
-    [SerializeField] private Vector3 lightningPos = new Vector3(0,0,0);
+    public GameObject Wrapper;
+
+    public ParticleSystem Lightning_Windup;
+    public ParticleSystem Lightning_Explosion;
+    public ParticleSystem Lightning_Residue;
+
+    public ParticleSystem test;
+
+    private Vector3 lightningPos;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            lightningEffect.SetVector3("SendPos", lightningPos);
+            StartCoroutine("LightningFull");
         }
+
+        if (Input.GetKeyDown(KeyCode.Q)) 
+        {
+            test.Play();
+        }
+    }
+
+    IEnumerator LightningFull()
+    {
+        var randomVector3 = new Vector3(Random.Range(-5, 5), 1, Random.Range(-5, 5));
+
+        Wrapper.SetActive(true);
+
+        Wrapper.transform.position = randomVector3;
+
+        Lightning_Windup.Play();
+
+        yield return new WaitForSeconds(5.3f);
+
+        lightningEffect.SetVector3("SendPos", randomVector3);
+        lightningEffect.Play();
+
+        Lightning_Explosion.Play();
+        Lightning_Windup.Stop();
+
+        yield return new WaitForSeconds(1f);
+
+        Lightning_Explosion.Stop();
+
+        yield return new WaitForSeconds(1f);
+
+        Lightning_Residue.Play();
+
+        Wrapper.SetActive(false);
     }
 }
